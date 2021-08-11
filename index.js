6d3e7817570d42ca5869c0b71ea0bfd7b6ee2720
小鳥圭介 <kei01301116@gmail.com>
2021-08-11 21:41:10 +0900

    多分動く

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const plantuml = require("node-plantuml");
 const fs = require("fs");
+const glob = require("glob");
 
-function generatePng(puFileName) {
-  fs.readFile(puFileName + ".pu", "utf-8", (err, data) => {
+// puファイルから.pngを生成する
+function generatePngFromPu(puFileNameWithoutExt) {
+  fs.readFile(puFileNameWithoutExt + ".pu", "utf-8", (err, data) => {
     if (err) throw err;
     console.log(data);
     const gen = plantuml.generate(data);
-    gen.out.pipe(fs.createWriteStream(puFileName + ".png"));
+    gen.out.pipe(fs.createWriteStream(puFileNameWithoutExt + ".png"));
   });
 }
-generatePng("sample/sample");
+
+// 拡張子を取り除く
+function trimExt(fileName) {
+  return fileName.split(".").slice(0, -1).join(".");
+}
+
+function main() {
+  glob("**/*", (err, fileNames) => {
+    fileNames.forEach((fileName) => {
+      if (fileName.endsWith(".pu")) {
+        const fileWithoutExt = trimExt(fileName);
+        //すでにpngが存在する場合、png作成処理スキップ
+        if (!fs.existsSync(fileWithoutExt + ".png")) {
+          generatePngFromPu(fileWithoutExt);
+        }
+      }
+    });
+  });
+}
+main();
